7b6aeef0723cf562977c94b87345332d1f8a23c1
Adrian Chalimoniuk <adrian.chalimoniuk@reckitt.com>
2024-01-13 10:10:00 +0100

    mobile view fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Search from '../components/Search/search';
 
 const pageStyles = {
   color: '#ffffff',
-  padding: 96,
+  padding: 16,
   fontFamily: '-apple-system, Roboto, sans-serif, serif',
   backgroundColor: '#3e3e42',
   minHeight: '100vh',
@@ -16,7 +16,9 @@ const IndexPage: FC<PageProps> = () => {
   const [searchedItem, setSearchedItem] = useState('Cheddar');
 
   return (
-    <main style={pageStyles}>
+    <main
+      style={pageStyles}
+      className='md:p-16'>
       <Search />
     </main>
   );
